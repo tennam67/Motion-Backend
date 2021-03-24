0d72df1e536d34e6e7b9a6c7a0190d861d1c84b9
Cindy Reber <cindy.reber@hispeed.ch>
2021-03-24 17:47:34 +0100

    backup post changes

--- backend/app/post/permissions.py
+++ backend/app/post/permissions.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method == 'GET':
             return True
-
-        return obj.user == request.user
+        elif obj.author and obj.author == request.user:
+            return True
--- backend/app/post/views.py
+++ backend/app/post/views.py
@@ -20,13 +20,14 @@ class RetrieveUpdateDestroyPost(RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     lookup_url_kwarg = 'post_id'
     lookup_field = 'id'
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwnerOrReadOnly]
 
 
 class ListUserPosts(ListAPIView):
     serializer_class = PostSerializer
     lookup_url_kwarg = 'user_id'
 
-    def get_queryset(self):
-        user_id = self.kwargs.get("user_id")
-        return Post.objects.filter(user__id=user_id).order_by("-created")
+    # def get_queryset(self):
+    #     user_id = self.kwargs.get("user_id")
+    #     return Post.objects.filter(user__id=user_id).order_by("-created")
+
